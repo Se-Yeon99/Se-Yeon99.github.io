28ef4b90f55bb326975b8bda30e0a4b586e222c8
[Se-Yeon99] <[hsy_0204@naver.com]>
2022-06-30 22:31:08 +0900

    audio추가

---
+++ js/music.js
@@ -0,0 +1,5 @@
+const audioContainer = document.querySelector("#audioContainer");
+const playBtn = document.querySelector('.js-playBtn');
+const stopBtn = document.querySelector('js-stopBtn');
+const audioNextBtn = document.querySelector('js-audioNextBtn');
+
